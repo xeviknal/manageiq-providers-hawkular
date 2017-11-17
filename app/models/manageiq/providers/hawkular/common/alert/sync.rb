79c7ef37bfa124b2f9daba560f088639c2a5b4fd
Xavier Canal i Masjuan <xeviknal@gmail.com>
2017-11-17 14:01:26 +0100

    WIP - not TDD-ed (oh gosh, sorry) Add worker to sync new resource to enterprise alert profiles

--- app/models/manageiq/providers/hawkular/common/alert/sync.rb
+++ app/models/manageiq/providers/hawkular/common/alert/sync.rb
@@ -5,6 +5,16 @@ module ManageIQ::Providers::Hawkular::Common::Alert::Sync
     after_create_commit :sync_alerts
   end
 
+  def sync_profile(alert_profile)
+    alert_profile.members.each do |alert|
+      sync_alert(alert)
+    end
+  end
+
+  def sync_alert(alert)
+    # Call ruby client
+  end
+
   private
 
   def sync_alerts
--- app/models/manageiq/providers/hawkular/middleware_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/hawkular/middleware_manager/refresh_worker/runner.rb
@@ -1,3 +1,10 @@
 class ManageIQ::Providers::Hawkular::MiddlewareManager::RefreshWorker::Runner <
   ManageIQ::Providers::BaseManager::RefreshWorker::Runner
+
+  def start
+    # resource - the new server|datasource|messaging
+    MiqAlertset.with_mode('MiddlewareServer').find_each do |alert_set|
+      resource.sync_profile(alert_profile)
+    end
+  end
 end
